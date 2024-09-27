5b8d627d302235d708f7a9f4635aa48ad1f32cb7
Pedro <desemhoph@hotmail.com>
2024-09-26 21:29:47 -0300

    1.3.5 Lucas Thiago

---
+++ src/templates/Template.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UMC_Nocoes_Projeto.src.templates
+{
+    public class Template
+    {
+        
+    }
+}
